4557f1dce0e87494ae3881c73752faf63a5cf601
igromanru <igromanru@yahoo.de>
2017-07-19 19:38:47 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,27 +1,54 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace BATTLEGROUNDS_EXERNAL
 {
     static class Program
     {
+        enum ProcessAccessType
+        {
+            PROCESS_ALL_ACCESS = PROCESS_CREATE_PROCESS | PROCESS_CREATE_THREAD | PROCESS_DUP_HANDLE | PROCESS_QUERY_INFORMATION |
+                                PROCESS_QUERY_LIMITED_INFORMATION | PROCESS_SET_INFORMATION | PROCESS_SET_QUOTA | PROCESS_SUSPEND_RESUME |
+                                PROCESS_TERMINATE | PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+            PROCESS_CREATE_PROCESS = 0x0080,
+            PROCESS_CREATE_THREAD = 0x0002,
+            PROCESS_DUP_HANDLE = 0x0040,
+            PROCESS_QUERY_INFORMATION = 0x0400,
+            PROCESS_QUERY_LIMITED_INFORMATION = 0x1000,
+            PROCESS_SET_INFORMATION = 0x0200,
+            PROCESS_SET_QUOTA = 0x0100,
+            PROCESS_SUSPEND_RESUME = 0x0800,
+            PROCESS_TERMINATE = 0x0001,
+            PROCESS_VM_OPERATION = 0x0008,
+            PROCESS_VM_READ = 0x0010,
+            PROCESS_VM_WRITE = 0x0020
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
         [STAThread]
         static void Main()
         {
+
+#if DEBUG
+            Process.EnterDebugMode();
+            Process[] processes = Process.GetProcessesByName("TslGame");
+            G.hProcess = OpenProcess((int)ProcessAccessType.PROCESS_ALL_ACCESS, false, processes[0].Id);
+#else
             if (Environment.GetCommandLineArgs().Length < 2)
             {
                 MessageBox.Show("No handle");
                 return;
             }
-
             // pExceptionHandler
             AppDomain.CurrentDomain.UnhandledException += (object sender, UnhandledExceptionEventArgs e) => MessageBox.Show(e.ExceptionObject.ToString());
 
             // Handle from hLeaker
             G.hProcess = (IntPtr)Convert.ToUInt32(Environment.GetCommandLineArgs()[1]);
-
+#endif
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new CheatForm());
